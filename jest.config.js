98177cd9686de266ca774e9294c8e3fa25939774
arina-kaa <Arina.kaa@inbox.ru>
2022-12-09 20:28:46 +0300

    Настроила окружение

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    collectCoverageFrom: ['./src/**'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 70,
+            lines: 80,
+        },
+    },
+};
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.ts',
+	devtool: 'inline-source-map',
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+};
